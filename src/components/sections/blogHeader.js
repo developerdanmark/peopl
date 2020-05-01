872089df8d3bd0f5148ab4302fa13a08566d117b
developerdanmark <developer@nordensoft.dk>
2020-05-02 01:14:03 +0300

    headers design

--- src/components/sections/blogHeader.js
+++ src/components/sections/blogHeader.js
@@ -6,29 +6,29 @@ const BlogHeader = (data) => {
     return (
         <>
             <div className="blog-banner">
-                <div className="container-fluid layer2">
+                <div className="layer1">
+                    <LazyLoad>
+                        <img src={data.data.image.asset.fluid.src} alt={data.data.title} />
+                    </LazyLoad>
+                </div>
+                <div className="container layer2 d-flex justify-content-center align-items-start flex-column">
                     <div className="row">
-                        <div className="col-md-7 d-flex justify-content-center align-items-center py-4 py-md-0 flex-column">
-                            <div className="w-md-75">
-                                <div className="tag"> {data.data.tag} </div>
-                                <div className="banner-title">{data.data.title}</div>
-                                {data.data._rawBody && <BlockContent blocks={data.data._rawBody} />}
-                                <div className="form-row inline-from w-100 py-5 pr-5">
-                                    <div className="form-group col-md-7">
-                                        <label htmlFor="b-email">Email</label>
-                                        <input type="email" id="b-email" className="form-control" />
-                                    </div>
-                                    <div className="form-group col-md-5">
-                                        <label htmlFor="name">&nbsp;</label>
-                                        <input type="submit" id="name" className="form-control" value="Tilmeld nyhedsbrev" />
-                                    </div>
+                        <div className="col-md-7 d-flex flex-column">
+                            <div className="tag"> {data.data.tag} </div>
+                            <div className="banner-title">{data.data.title}</div>
+                            {data.data._rawBody && <BlockContent blocks={data.data._rawBody} />}
+                            <div className="form-row inline-from w-100 py-5 pr-5">
+                                <div className="form-group col-md-7">
+                                    <label htmlFor="b-email">Email</label>
+                                    <input type="email" id="b-email" className="form-control" />
+                                </div>
+                                <div className="form-group col-md-5">
+                                    <label htmlFor="name">&nbsp;</label>
+                                    <input type="submit" id="name" className="form-control" value="Tilmeld nyhedsbrev" />
                                 </div>
                             </div>
                         </div>
                         <div className="col-md-5 d-flex justify-content-center align-items-end pr-md-0 flex-column">
-                            <LazyLoad>
-                                <img src={data.data.image.asset.fluid.src} alt={data.data.title} />
-                            </LazyLoad>
                         </div>
                     </div>
                 </div>
--- src/components/sections/referencesHeader.js
+++ src/components/sections/referencesHeader.js
@@ -7,20 +7,21 @@ const ReferencesHeader = (data) => {
     return (
         <>
             <div className="blog-banner">
-                <div className="container-fluid layer2">
+                <div className="layer1">
+                    <LazyLoad>
+                        <img src={data.data.image.asset.fluid.src} alt={data.data.title} />
+                    </LazyLoad>
+                </div>
+                <div className="container layer2 d-flex justify-content-center align-items-start py-4 py-md-0 flex-column">
                     <div className="row">
-                        <div className="col-md-7 d-flex justify-content-center align-items-center pl-md-5 py-4 py-md-0 flex-column">
-                            <div className="w-md-75">
-                                <div className="tag"> {data.data.tag} </div>
-                                <div className="banner-title">{data.data.title}</div>
-                                {data.data._rawBody && <BlockContent blocks={data.data._rawBody} />}
-                                <Link className="btn btn-peopl">Vil du høre mere?</Link>
-                            </div>
+                        {/*  */}
+                        <div className="col-md-7">
+                            <div className="tag"> {data.data.tag} </div>
+                            <div className="banner-title">{data.data.title}</div>
+                            {data.data._rawBody && <BlockContent blocks={data.data._rawBody} />}
+                            <Link className="btn btn-peopl">Vil du høre mere?</Link>
                         </div>
                         <div className="col-md-5 d-flex justify-content-center align-items-end pr-md-0 flex-column">
-                            <LazyLoad>
-                                <img src={data.data.image.asset.fluid.src} alt={data.data.title} />
-                            </LazyLoad>
                         </div>
                     </div>
                 </div>
