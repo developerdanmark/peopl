46a9432a54d450ad299433db92913dbecfdc974b
developerdanmark <developer@nordensoft.dk>
2020-05-01 03:03:18 +0300

    lazyload video

--- src/components/sections/videoSection.js
+++ src/components/sections/videoSection.js
@@ -8,28 +8,30 @@ const VideoSection = (data) => {
         <>
             {/* ########### Video Block ########### */}
 
-            <div className="video-block py-5" >
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12 py-5">
-                            <div className="video-block-content mx-auto">
-                                <div class="embed-responsive embed-responsive-16by9">
-                                    <LazyLoad>
+            <LazyLoad offset={-300}>
+                <div className="video-block py-5" >
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12 py-5">
+
+                                <div className="video-block-content mx-auto">
+                                    <div class="embed-responsive embed-responsive-16by9">
+
                                         <iframe class="embed-responsive-item" title="245% stigning fra SEO" src={embedUrl} allowfullscreen></iframe>
-                                    </LazyLoad>
+                                    </div>
+                                    <div className="video-title">
+                                        {data.data.title}
+                                    </div>
+                                    <p className="video-description mx-auto">
+                                        {data.data.description}
+                                    </p>
+                                    {data.data.buttonText && <Link to={data.data.buttonLink} className="btn btn-outline-secondary video-button"> {data.data.buttonText} </Link>}
                                 </div>
-                                <div className="video-title">
-                                    {data.data.title}
-                                </div>
-                                <p className="video-description mx-auto">
-                                    {data.data.description}
-                                </p>
-                                {data.data.buttonText && <Link to={data.data.buttonLink} className="btn btn-outline-secondary video-button"> {data.data.buttonText} </Link>}
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </LazyLoad>
         </>
 
     )
