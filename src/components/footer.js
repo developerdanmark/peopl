8c7f70bca926ce6b1ff4ab255a2bb5292240490c
developerdanmark <developer@nordensoft.dk>
2020-05-02 15:12:43 +0300

    footer social icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -35,9 +35,19 @@ const Footer = ({ data }) => {
                   _id
                   _type
                   title
-                  linkedin
-                  facebook
-                  instagram
+                  social {
+                    _type
+                    title
+                    link
+                    icon {
+                      asset {
+                        fluid {
+                          src
+                          srcWebp
+                        }
+                      }
+                    }
+                  }
                   link {
                     title
                     slug {
@@ -140,94 +150,89 @@ const Footer = ({ data }) => {
       </div>
 
       <footer>
-      <div className="container py-5">
-        <div className="row">
-          <div className="col-md-3">
-            <h5> {footerData.f1.edges.map((q, i) => {
-              return (q.node.title)
-            })} </h5>
+        <div className="container py-5">
+          <div className="row">
+            <div className="col-md-3">
+              <h5> {footerData.f1.edges.map((q, i) => {
+                return (q.node.title)
+              })} </h5>
 
-            <ul className="list-unstyled text-small">
-              {footerData.f1.edges.map((q, i) => {
-                return (
-                  q.node.link.map((n, i) => {
-                    return (
-                      <li><Link className="text-muted" to={n.slug.current}> {n.title} </Link></li>
-                    )
-                  })
-                )
-              })}
-            </ul>
-          </div>
-          <div className="col-6 col-md-3 mb-5 mb-md-0 d-flex flex-nowrap flex-column">
-            {footerData.f2.edges.map((q, i) => {
-              return (
-                <div className="order-2 order-md-1">
-                  <h5>{q.node.title}</h5>
-                  <div className="social-icons">
-                    <a href={q.node.linkedin} target="_blank" rel="noopener noreferrer">
-                      <Img fluid={footerData.linkedinIcon.childImageSharp.fluid} className="linkedin-icon" />
-                    </a>
-                    <a href={q.node.facebook} target="_blank" rel="noopener noreferrer">
-                      <Img fluid={footerData.facebookIcon.childImageSharp.fluid} className="facebook-icon" />
-                    </a>
-                    <a href={q.node.instagram} target="_blank" rel="noopener noreferrer">
-                      <Img fluid={footerData.instagramIcon.childImageSharp.fluid} className="instagram-icon" />
-                    </a>
-                  </div>
-                </div>
-              )
-            })}
-
-            <ul className="list-unstyled text-small order-1 order-md-2">
-              <li><a className="text-muted" href="/sitemap.xml" target="_blank" rel="noopener noreferrer" >Sitemap</a></li>
-              {footerData.f2.edges.map((q, i) => {
-                return (
-                  q.node.link.map((n, i) => {
-                    return (
-                      <li><Link className="text-muted" to={n.slug.current}> {n.title} </Link></li>
-                    )
-                  })
-                )
-              })}
-            </ul>
-          </div>
-          <div className="col-6 col-md-3 mb-5 mb-md-0">
-            {footerData.f3.edges.map((q, i) => {
-              return (
-                <>
-                  <h5>{q.node.title}</h5>
-                  {q.node._rawText && <BlockContent blocks={q.node._rawText} />}
-                </>
-              )
-            })}
-          </div>
-          <div className="col-md-3">
-            <div id="map-container-google-1" class="z-depth-1-half map-container">
-                <LazyLoad>
-                {footerData.f4.edges.map((q, i) => {
-                  let lat = Number(q.node.mapLat)
-                  let lng = Number(q.node.mapLong)
-                  let zoom = Number(q.node.zoom)
-                  let key = q.node.key
+              <ul className="list-unstyled text-small">
+                {footerData.f1.edges.map((q, i) => {
                   return (
-                    <>
-                      <SimpleMap key={i} center={ {lat: lat , lng: lng }} zoom={zoom} apiKey={key} />
-                    </>
+                    q.node.link.map((n, i) => {
+                      return (
+                        <li><Link className="text-muted" to={n.slug.current}> {n.title} </Link></li>
+                      )
+                    })
                   )
                 })}
-                  
-                </LazyLoad>
+              </ul>
             </div>
+            <div className="col-6 col-md-3 mb-5 mb-md-0 d-flex flex-nowrap flex-column">
+              <div className="order-2 order-md-1">
+                <h5>{footerData.f2.edges[0].node.title}</h5>
+                {/* {JSON.stringify(footerData.f2.edges[0].node.social)} */}
+                <div className="social-icons">
+                  {footerData.f2.edges[0].node.social.map((q, i) => {
+                    return (
+                      <a href={q.link} target="_blank" rel="noopener noreferrer">
+                        <Img fluid={q.icon.asset.fluid} alt={q.title} className="social-icon" />
+                      </a>
+                    )
+                  })}
+                </div>
+              </div>
 
-          </div>
-          <div className="col-md-12">
-            <div className="logo-block mt-4">
-              <Image />
+              <ul className="list-unstyled text-small order-1 order-md-2">
+                <li><a className="text-muted" href="/sitemap.xml" target="_blank" rel="noopener noreferrer" >Sitemap</a></li>
+                {footerData.f2.edges.map((q, i) => {
+                  return (
+                    q.node.link.map((n, i) => {
+                      return (
+                        <li><Link className="text-muted" to={n.slug.current}> {n.title} </Link></li>
+                      )
+                    })
+                  )
+                })}
+              </ul>
+            </div>
+            <div className="col-6 col-md-3 mb-5 mb-md-0">
+              {footerData.f3.edges.map((q, i) => {
+                return (
+                  <>
+                    <h5>{q.node.title}</h5>
+                    {q.node._rawText && <BlockContent blocks={q.node._rawText} />}
+                  </>
+                )
+              })}
+            </div>
+            <div className="col-md-3">
+              <div id="map-container-google-1" class="z-depth-1-half map-container">
+                <LazyLoad>
+                  {footerData.f4.edges.map((q, i) => {
+                    let lat = Number(q.node.mapLat)
+                    let lng = Number(q.node.mapLong)
+                    let zoom = Number(q.node.zoom)
+                    let key = q.node.key
+                    return (
+                      <>
+                        <SimpleMap key={i} center={{ lat: lat, lng: lng }} zoom={zoom} apiKey={key} />
+                      </>
+                    )
+                  })}
+
+                </LazyLoad>
+              </div>
+
+            </div>
+            <div className="col-md-12">
+              <div className="logo-block mt-4">
+                <Image />
+              </div>
             </div>
           </div>
         </div>
-      </div>
       </footer>
     </>
   )
