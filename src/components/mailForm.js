2e9cf3bfe1b6d86ddd6fa2ae42550083337139e7
developerdanmark <developer@nordensoft.dk>
2020-05-07 14:33:33 +0300

    form test

--- src/components/mailForm.js
+++ src/components/mailForm.js
@@ -1,14 +1,52 @@
 import React from 'react'
 
+
+// constructor(props) {
+//     super(props)
+//     this.ContactForm = React.createRef()
+//     this.state = {
+//       name: "",
+//       email: "",
+//       message: "",
+//     }
+//   }
+
+// encode = data => {
+//     return Object.keys(data)
+//       .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+//       .join("&")
+//   }
+
+// const handleSubmit = event => {
+//     event.preventDefault()
+//     const form = this.ContactForm.current
+//     fetch("/", {
+//       method: "POST",
+//       headers: { "Content-Type": "application/x-www-form-urlencoded" },
+//       body: this.encode({
+//         "form-name": form.getAttribute("name"),
+//         ...this.state,
+//       }),
+//     })
+//       .then(() => navigate("/"))
+//       .catch(error => alert(error))
+
+//     this.setState({
+//       name: "",
+//       email: "",
+//       message: "",
+//     })
+//   }
+
 const MailForm = () => {
     return (
         <form className="banner-form" name="contact" method="POST" data-netlify="true">
             <div className="form-title">
                 Skal vi også vækste <br /> din virksomhed?
-                  </div>
+            </div>
             <div className="form-subtitle">
                 Lad os invitere dig ind til et <br /> uforpligtende møde
-                  </div>
+            </div>
             <div className="form-group">
                 <label htmlFor="name">Navn</label>
                 <input type="text" className="form-control" id="name" name="name" />
