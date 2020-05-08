8cba8ea3448c813d193e8c46d018981ebab2d760
developerdanmark <developer@nordensoft.dk>
2020-05-08 04:11:27 +0300

    mail test

--- src/components/mailForm.js
+++ src/components/mailForm.js
@@ -32,34 +32,32 @@ class MailForm extends Component {
 
     render() {
         return (
-            <div className="col-sm-4 offset-sm-4">
-                <form className="banner-form" name="contact" method="POST" onSubmit={this.handleSubmit.bind(this)}>
-                    <input type="hidden" name="bot-field" />
-                    <div className="form-title">
-                        Skal vi også vækste <br /> din virksomhed?
+            <form className="banner-form" name="contact" method="POST" netlify>
+                <input type="hidden" name="bot-field" />
+                <div className="form-title">
+                    Skal vi også vækste <br /> din virksomhed?
                     </div>
-                    <div className="form-subtitle">
-                        Lad os invitere dig ind til et <br /> uforpligtende møde
+                <div className="form-subtitle">
+                    Lad os invitere dig ind til et <br /> uforpligtende møde
                     </div>
-                    <div className="form-group">
-                        <label htmlFor="name">Navn</label>
-                        <input type="text" className="form-control" id="name" name="name" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="phone">Telefon</label>
-                        <input type="tel" className="form-control" id="phone" name="phone" />
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" className="form-control" id="email" name="email" aria-describedby="emailHelp" />
-                    </div>
-                    <button type="submit" className="btn btn-banner-form">Bliv kontaktet</button>
-                </form>
-            </div>
+                <div className="form-group">
+                    <label htmlFor="name">Navn</label>
+                    <input type="text" className="form-control" id="name" name="name" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="phone">Telefon</label>
+                    <input type="tel" className="form-control" id="phone" name="phone" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" className="form-control" id="email" name="email" aria-describedby="emailHelp" />
+                </div>
+                <button type="submit" className="btn btn-banner-form">Bliv kontaktet</button>
+            </form>
         )
     }
 }
-
+// onSubmit={this.handleSubmit.bind(this)}
 export default MailForm;
 
 // const MailForm = () => {
